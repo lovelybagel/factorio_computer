9a54507fba96de0183583587c987df8794f23ee6
Natalie Brooks <natalie3brooks@gmail.com>
2021-04-24 20:48:25 -0700

    added programs folder with basic program for testing

---
+++ Programs/fibonacci.asm
@@ -0,0 +1,15 @@
+ST  1   513
+ST  1   770
+top
+LD  0   513
+ADD 0   514
+ST  0   515
+LD  0   514
+ST  0   513
+LD  0   515
+ST  0   514
+LD  0   513
+ST  0   769
+LD  0   514
+ST  0   770
+JMP top 0
